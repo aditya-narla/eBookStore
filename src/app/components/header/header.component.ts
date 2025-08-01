85c5ad179875363a9f052ac95d1e762c95c109a8
Aditya Narla <narla.a@northeastern.edu>
2025-07-31 21:01:07 -0400

    Complete two-tier header structure with top-menu and main-menu components

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [TopMenuComponent, MainMenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  // Component properties - these can be used in the template
-  storeName: string = 'eBookStore';
-  cartCount: number = 0;
-  
-  // We could add methods here later for handling navigation clicks
+
 }
---
+++ src/app/components/header/main-menu/main-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-menu',
+  imports: [],
+  templateUrl: './main-menu.component.html',
+  styleUrl: './main-menu.component.css'
+})
+export class MainMenuComponent {
+  storeName = 'eBookStore';
+  cartCount = 3;
+}
