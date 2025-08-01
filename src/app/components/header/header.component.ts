b1c2ec1000313d856dfe2feb56629a6a82fcd236
Aditya Narla <narla.a@northeastern.edu>
2025-07-31 20:34:23 -0400

    Topic 1: Components - Built out header component with logo, navigation, and styling

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  // Component properties - these can be used in the template
+  storeName: string = 'eBookStore';
+  cartCount: number = 0;
+  
+  // We could add methods here later for handling navigation clicks
 }
